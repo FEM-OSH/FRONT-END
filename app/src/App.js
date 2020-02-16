96ee194a2d7a0052dd566ca4650d3fb59a9886c3
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-02-16 17:20:37 +0000

    Hard coding get started section

--- app/src/App.js
+++ app/src/App.js
@@ -32,12 +32,49 @@ function App() {
         <h2>Lending platform</h2>
         <p>
           Providing you easy access to funds from family and friends when you
-          must need it.
+          most need it.
         </p>
         <Button small background={colors.tertiary} fontColor={colors.secondary}>
           Get Started
         </Button>
       </div>
+      <div>
+        <button>How To Get Started </button>
+        <div>
+          <div>
+            <div>Create your account</div>
+            <p>
+              Sign Up at the top left corner of this page,verify your email and
+              login.{" "}
+            </p>
+          </div>
+
+          <div>
+            <div>Add family &amp; friends</div>
+            <p>
+              Add your people who would be able to render help in your time of
+              need.{" "}
+            </p>
+          </div>
+
+          <div>
+            <div>Take our credit test</div>
+            <p>
+              This is more of a survey that contains details for your profile
+              and access your credit rating.{" "}
+            </p>
+          </div>
+
+          <div>
+            <div>Request/accept loan request</div>
+            <p>
+              Request for loan from friend, accept/reject an already initiated
+              request
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>That friend who never repays his debt</div>
     </div>
   );
 }
