b1e4677b15773e94cef1ce11c2a11af212de65d5
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-02-16 17:33:26 +0000

    Conclude hardcoded template

--- app/src/App.js
+++ app/src/App.js
@@ -75,6 +75,41 @@ function App() {
         </div>
       </div>
       <div>That friend who never repays his debt</div>
+      <div>Lessons learnt from my sad experience</div>
+      <p>
+        Danotious is a good friend of mine, we grew up in the same
+        neighbourhood, had lots of fun moments and we sure did consider
+        ourselves brothers rather than just friends.
+      </p>
+      <p>
+        Time and distance had come between us as we ventured to face life, only
+        to reconnect and continue as though we were never separated. One
+        faithful morning, Danotious came calling and he did seek for my help. He
+        said he needed a loan, he had some bills to settle and would need
+        N30,000. What are we friends for, if I cannot help when I am empowered
+        to do so. I borrowed him the amount requested and asked when he would
+        repay me. He muffled something about reimbursing me at the end of the
+        month when he receives his salary. I took him by his word and asked when
+        it was month end. Then the trouble began, he would post me to a later
+        date, started avoiding my calls, changed his known pattern of movement
+        and till date, i have not received my money back.
+      </p>
+      <p>
+        I could do nothing to him than let him go, but, it struck me to realise
+        how many people he could as well be indebted to at the moment.
+      </p>
+      <p>
+        He is a fraudstar and such persons who are not honest and true to their
+        word should be avoided by all means necessary.
+      </p>
+      <div>Avoid lending money to the wrong hands... Join lender today</div>
+      <div>Rate credit worthiness and avoid fraudsters</div>
+      <div>Available on</div>
+
+      {/* <Footer>
+        Borrow>>> <br/>
+        Lend>>>
+      </Footer> */}
     </div>
   );
 }
